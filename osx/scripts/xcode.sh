2d2f1d2f5728717fea0b093d6efd8cb6e78fcd9e
Georgii Gorbachev <hey.gorbachev@gmail.com>
2021-03-06 18:23:47 +0100

    OSX: Update XCode stuff

--- osx/scripts/xcode.sh
+++ osx/scripts/xcode.sh
@@ -4,11 +4,10 @@
 # https://stackoverflow.com/questions/17980759/xcode-select-active-developer-directory-error/17980786#17980786
 
 # 1. Install Xcode from the App Store (or get it from https://developer.apple.com/xcode/).
+# Make sure Xcode app is in the /Applications directory (NOT /Users/{user}/Applications).
 
-# 2. Accept the Terms and Conditions.
-sudo xcodebuild -license
-
-# 3. Ensure Xcode app is in the /Applications directory (NOT /Users/{user}/Applications).
-
-# 4. Point xcode-select to the Xcode app Developer directory
+# 2. Point xcode-select to the Xcode app Developer directory.
 sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+
+# 3. Accept the Terms and Conditions.
+sudo xcodebuild -license

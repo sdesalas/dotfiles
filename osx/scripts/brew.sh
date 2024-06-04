cab64cddfab5ef4a62078a374ae77ecb5c52a3ab
Georgii Gorbachev <hey.gorbachev@gmail.com>
2024-06-04 21:26:43 +0200

    OSX: Node

--- osx/scripts/brew.sh
+++ osx/scripts/brew.sh
@@ -92,9 +92,11 @@ nvm install 18.17.0         # Kibana
 nvm install 18.17.1         # Kibana
 nvm install 18.18.2         # Kibana
 nvm install 20.10.0         # Kibana
-nvm install 20.11.1         # Kibana current
-nvm alias default 20.11.1 && nvm use default
-# nvm reinstall-packages 20.10.0
+nvm install 20.11.1         # Kibana
+nvm install 20.12.2         # Kibana
+nvm install 20.13.1         # Kibana current
+nvm alias default 20.13.1 && nvm use default
+# nvm reinstall-packages 20.12.2
 nvm ls
 
 # How to upgrade to a newer Node version:
